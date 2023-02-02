54bff7d0275919e2ecba845eb1c55cf2deccd4ac
unknown <raflymhndra@gmail.com>
2023-02-03 02:28:29 +0700

    add some style to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import Search from '../Search/Search'
 
 function Navbar() {
     return (
-        <div className='container text-center sm:text-left sm:mx-auto py-6 flex justify-between'>
+        <div className='container w-full sm:mx-auto py-6 flex justify-between'>
             <a href='/' className='text-center sm:text-left text-2xl font-bold tracking-widest uppercase text-yellow-400'>anime studios</a>
         </div>
     )
